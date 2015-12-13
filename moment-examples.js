896da42212e36041f66f1d4e3c7358bce0557b3a
Tony Nguyen <dolongky@gmail.com>
2015-12-13 15:04:48 +0700

    Add name support

--- moment-examples.js
+++ moment-examples.js
@@ -1,15 +1,19 @@
 var moment = require('moment');
-var now = moment();
+var now = moment().subtract(1, 'year').valueOf();
+
+var timestampMoment = moment.utc(now);
+
+console.log(timestampMoment.format('YYYY-M-DDThh:mm:ss.SSS'));
 
 // console.log(now.format());
 // console.log(now.format('X'));
 // console.log(now.valueOf());
 
 
-var timestamp = 1449803161146;
-var timestampMoment = moment.utc(timestamp);
+// var timestamp = 1449803161146;
+// var timestampMoment = moment.utc(timestamp);
 
-console.log(timestampMoment.local().format('h:mm a'));
+// console.log(timestampMoment.local().format('YYYY-M-DDTh:mm:ss.SSS'));
 
 
 // now.subtract(1, 'year');
--- server.js
+++ server.js
@@ -13,11 +13,12 @@ io.on('connection', function(socket) {
     socket.on('message', function(message) {
         console.log('Message received: ' + message.text);
 
-        message.timestamp = moment().valueOf();
+        // message.timestamp = moment().valueOf();
         io.emit('message', message);
     });
 
     socket.emit('message', {
+        name: 'System',
         text: 'Welcome to the chat application!',
         timestamp: moment().valueOf()
     });
